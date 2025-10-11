b609c50680e1ef38cb19edefc2315ac11adbb4ae
chvyshanvi <n210759@rguktn.ac.in>
2025-10-11 13:56:15 +0530

    hghg

--- src/main/java/com/project2/To_do_list/ToDoListApplication.java
+++ src/main/java/com/project2/To_do_list/ToDoListApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ToDoListApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
